21181a976f89933a01f58871c527b9d10cf5664b
dsanmartinares <dsanmartin@kaizen.com>
2026-09-22 20:56:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE  = 'analista-pechuga-v3.2';
+const CACHE  = 'analista-pechuga-v4.0';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
